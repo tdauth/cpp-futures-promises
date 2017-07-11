e83b6482a93ca1c72ccf170f8da20490b572defa
barade <barade.barade@web.de>
2017-07-11 19:44:11 +0200

    Generate XML file for unit tests

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ cd ./build
 # Create a "compile_commands.json" file for analysis: http://eli.thegreenplace.net/2014/05/21/compilation-databases-for-clang-based-tools
 cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang"
 make -j4
-ctest
+ctest -T test
 cpack -G "RPM" .
