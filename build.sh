70346c12be3b66ef17d2140043df48c70b5cbb31
barade <barade.barade@web.de>
2017-07-23 14:24:06 +0200

    Set module path

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ if [ ! -d "./build" ] ; then
 	mkdir ./build
 fi
 
-CMAKE_PREFIX_PATH="$(pwd)/cmake"
-echo "Using prefix path: $CMAKE_PREFIX_PATH"
+CMAKE_MODULE_PATH="$(pwd)/cmake"
+echo "Using prefix path: $CMAKE_MODULE_PATH"
 echo "It contains the following files:"
-ls -lha "$CMAKE_PREFIX_PATH"
+ls -lha "$CMAKE_MODULE_PATH"
 cd ./build
 # Clang cannot be used due to errors with Folly: https://github.com/facebook/folly/issues/555
 # Create a "compile_commands.json" file for analysis: http://eli.thegreenplace.net/2014/05/21/compilation-databases-for-clang-based-tools
-cmake ../ -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang"
+cmake ../ -DCMAKE_MODULE_PATH="$CMAKE_MODULE_PATH" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang"
 make -j4
 rm -r "$WORKSPACE/cpp-futures-promises/workspace/build/Testing" || true
 ctest -T test
