2026ccbb86fee179b1f3015b73c9ed7916266669
barade <barade.barade@web.de>
2017-07-24 10:42:43 +0200

    Use GCC in CMake

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ ls -lha "$CMAKE_MODULE_PATH"
 cd ./build
 # Clang cannot be used due to errors with Folly: https://github.com/facebook/folly/issues/555
 # Create a "compile_commands.json" file for analysis: http://eli.thegreenplace.net/2014/05/21/compilation-databases-for-clang-based-tools
-cmake ../ -DCMAKE_MODULE_PATH="$CMAKE_MODULE_PATH" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang" -DCMAKE_CXX_COMPILER="/usr/bin/clang++" -DCMAKE_C_COMPILER="/usr/bin/clang"
+cmake ../ -DCMAKE_MODULE_PATH="$CMAKE_MODULE_PATH" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DCMAKE_C_COMPILER="/usr/bin/gcc"
 make -j4
 rm -r "$WORKSPACE/cpp-futures-promises/workspace/build/Testing" || true
 ctest -T test
