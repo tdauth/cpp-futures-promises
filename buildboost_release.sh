3bcf57cffb06ef7d36935bf916b7020b049b870a
barade <barade.barade@web.de>
2017-07-30 21:11:33 +0200

    Completely remove the release versions to simplify the project

--- buildboost_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$1"
-./b2 dll-path="$2" cxxflags="$CXXFLAGS -O3" -j4 toolset=gcc variant=release install
-# TODO apparently bjam ignores the rpath on installing and Folly fails at the linking step with the Boost C++ Libraries since to rpaths are set.
-echo "Changing rpaths of Boost libraries:"
-cd "$2"
-echo "Listing all files:"
-ls -lha
