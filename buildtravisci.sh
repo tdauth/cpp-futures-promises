2b706d78cb176cb5cd6318d4d32484cb481ff686
Tamino Dauth <tamino@cdauth.eu>
2018-09-21 16:14:53 +0200

    Decrease build time by building only coverage mode with TravisCI

--- buildtravisci.sh
+++ buildtravisci.sh
@@ -2,14 +2,8 @@
 echo "Installing dependencies on Fedora:"
 bash ./install_fedora_dependencies.sh
 
-echo "Building project in debug mode:"
-bash ./build.sh
-
 echo "Building project in coverage mode:"
 bash ./buildcoverage.sh
-
-echo "Building project in release mode:"
-bash ./buildrelease.sh
 
 echo "Cppcheck analysis:"
 bash ./cppcheck.sh
