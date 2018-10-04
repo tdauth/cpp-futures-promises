01a82e9f5a61263c699562c22a73a7dbc70fa419
Tamino Dauth <tamino@cdauth.eu>
2018-10-04 15:08:24 +0200

    Install autotools on Docker

--- install_fedora_dependencies.sh
+++ install_fedora_dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dnf -y install git cmake gcc-c++ libatomic bash which rpm-build gflags-devel glog-devel valgrind autoconf automake libtool zlib-devel lzma-devel snappy-devel double-conversion-devel openssl-devel libevent-devel lcov cppcheck cppcheck-htmlreport
+dnf -y install git  autoconf automake cmake gcc-c++ libatomic bash which rpm-build gflags-devel glog-devel valgrind autoconf automake libtool zlib-devel lzma-devel snappy-devel double-conversion-devel openssl-devel libevent-devel lcov cppcheck cppcheck-htmlreport
