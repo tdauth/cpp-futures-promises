5aac08d0f4e054c2c6f22910850f4a6eff28d2f3
Tamino Dauth <tamino@cdauth.eu>
2019-02-10 15:59:50 +0100

    Show Folly build errors

--- buildcoverage.sh
+++ buildcoverage.sh
@@ -22,6 +22,8 @@ make -j1 # Use one job to improve error detection and exit early.
 if [ "$?" -ne 0 ] ; then
     echo "Folly configure logs:"
     cat "./folly-prefix/src/folly-stamp/folly-configure-"*.log
+    echo "Folly build logs:"
+    cat "./folly-prefix/src/folly-stamp/folly-build-"*.log
 
 	exit 1
 fi
